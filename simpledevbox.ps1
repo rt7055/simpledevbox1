8900674c75268f76967f1b2861c05f4b0ebb1c22
rt7055 <47331254+rt7055@users.noreply.github.com>
2019-02-06 10:48:23 -0500

    Update simpledevbox.ps1

--- simpledevbox.ps1
+++ simpledevbox.ps1
@@ -5,4 +5,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install visualstudiocode -y
 choco install git -y 
 choco install nodejs-lts  -y
+choco install docker-compose -y
 
