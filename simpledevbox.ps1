0e260d1786afdb3243123240359fb1f36139266e
rt7055 <47331254+rt7055@users.noreply.github.com>
2019-03-06 10:15:35 -0500

    Update simpledevbox.ps1

--- simpledevbox.ps1
+++ simpledevbox.ps1
@@ -5,4 +5,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install googlechrome -y
 choco install firefox -y
 
+#Install AD future on azure VM's
+	
+Add-WindowsFeature RSAT-AD-PowerShell
 
